d2afd7a13907bb7f08b1605d20bc3b814104bc67
brunorosselli <64792644+brunorosselli@users.noreply.github.com>
2024-05-18 15:09:14 -0300

    [gReporter-01] Create Project (#1)

---
+++ src/main/java/com/example/gamer/report/GamerReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamer.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamer/report/GamerReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamer.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
